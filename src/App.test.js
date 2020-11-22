e185065543f7dba2dc96cf7aa944781b27bc3409
Zain Fathoni <zain.fathoni@gmail.com>
2020-11-22 16:59:34 +0800

    Ensure to clean up the test environment before each test

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,22 @@
 import ReactDOM from 'react-dom';
 import App from './App';
 
-test('renders learn react link', () => {
+beforeEach(() => {
+  document.body.innerHTML = ''
+})
+
+test('renders form correctly', () => {
+  const div = document.createElement('div')
+  document.body.append(div)
+
+  ReactDOM.render(<App />, div)
+
+  const [nama, nik] = div.querySelectorAll('form > div > div')
+  expect(nama.textContent).toBe('Nama')
+  expect(nik.textContent).toBe('NIK')
+});
+
+test('form submission', () => {
   const div = document.createElement('div')
   document.body.append(div)
 
