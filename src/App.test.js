0f02bf1bf75cdd0458ba84ef5d84d9a18d42b0b6
Zain Fathoni <zain.fathoni@gmail.com>
2020-11-22 17:07:38 +0800

    Start using @testing-library/react

--- src/App.test.js
+++ src/App.test.js
@@ -1,28 +1,13 @@
-import ReactDOM from 'react-dom';
+import { render, screen } from '@testing-library/react'
 import App from './App';
 
-beforeEach(() => {
-  document.body.innerHTML = ''
-})
-
 test('renders form correctly', () => {
-  const div = document.createElement('div')
-  document.body.append(div)
+  render(<App />)
 
-  ReactDOM.render(<App />, div)
-
-  const [nama, nik] = div.querySelectorAll('form > div > div')
-  expect(nama.textContent).toBe('Nama')
-  expect(nik.textContent).toBe('NIK')
+  const nama = screen.getByText('Nama')
+  expect(nama).toBeDefined()
+  
+  const nik = screen.getByText('NIK')
+  expect(nik).toBeDefined()
 });
 
-test('form submission', () => {
-  const div = document.createElement('div')
-  document.body.append(div)
-
-  ReactDOM.render(<App />, div)
-
-  const [nama, nik] = div.querySelectorAll('form > div > div')
-  expect(nama.textContent).toBe('Nama')
-  expect(nik.textContent).toBe('NIK')
-});
