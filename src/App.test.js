107785989821bd1a7414b300cb79f7e2418c3685
Zain Fathoni <zain.fathoni@gmail.com>
2020-11-22 16:49:06 +0800

    Test App.js using plain JavaScript & ReactDOM

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
-import { render, screen } from '@testing-library/react';
+import ReactDOM from 'react-dom';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const div = document.createElement('div')
+  document.body.append(div)
+
+  ReactDOM.render(<App />, div)
+
+  const [nama, nik] = div.querySelectorAll('form > div > div')
+  expect(nama.textContent).toBe('Nama')
+  expect(nik.textContent).toBe('NIK')
 });
